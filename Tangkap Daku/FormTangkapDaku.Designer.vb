cb26906fee67bdf1a30f235dcc1e3c8897df1661
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2020-05-21 23:45:59 +0700

    Add project files.

---
+++ Tangkap Daku/FormTangkapDaku.Designer.vb
@@ -0,0 +1,58 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormTangkapDaku
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tombolTangkapDaku = New System.Windows.Forms.Button()
+        Me.tmrGame = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'tombolTangkapDaku
+        '
+        Me.tombolTangkapDaku.Location = New System.Drawing.Point(116, 128)
+        Me.tombolTangkapDaku.Name = "tombolTangkapDaku"
+        Me.tombolTangkapDaku.Size = New System.Drawing.Size(87, 23)
+        Me.tombolTangkapDaku.TabIndex = 0
+        Me.tombolTangkapDaku.Text = "TangkapDaku"
+        Me.tombolTangkapDaku.UseVisualStyleBackColor = True
+        '
+        'tmrGame
+        '
+        Me.tmrGame.Enabled = True
+        Me.tmrGame.Interval = 1000
+        '
+        'FormTangkapDaku
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(335, 296)
+        Me.Controls.Add(Me.tombolTangkapDaku)
+        Me.Name = "FormTangkapDaku"
+        Me.Text = "Tangkap Daku"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents tombolTangkapDaku As System.Windows.Forms.Button
+    Friend WithEvents tmrGame As System.Windows.Forms.Timer
+
+End Class
---
+++ Tangkap Daku/FormTangkapDaku.vb
@@ -0,0 +1,26 @@
+﻿Public Class FormTangkapDaku
+
+    Private Sub tmrGame_Tick(sender As Object, e As EventArgs) Handles tmrGame.Tick
+        'menciptakan sebuah objek Random 
+        Dim rand As New Random
+
+        'variabel-variabel untuk memuat koordinat XY
+        Dim intKiriBaru, intAtasBaru As Integer
+
+        'membaca koordinat XY acak
+        intKiriBaru = rand.Next(Me.Width - tombolTangkapDaku.Width)
+        intAtasBaru = rand.Next(Me.Height - tombolTangkapDaku.Height)
+
+        'menggeser tombol ke lokasi baru
+        tombolTangkapDaku.Left = intKiriBaru
+        tombolTangkapDaku.Top = intAtasBaru
+    End Sub
+
+    Private Sub tombolTangkapDaku_Click(sender As Object, e As EventArgs) Handles tombolTangkapDaku.Click
+        'menonaktifkan timer
+        tmrGame.Enabled = False
+
+        'menampilkan pesa
+        MessageBox.Show("Anda menang!")
+    End Sub
+End Class
